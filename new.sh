94ae2cd2b70849d4deb310b8502d4b0892ab5f0b
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-08-31 12:52:02 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update package repositories
+sudo apt update
+sudo apt upgrade -y
+
+# Install Java 11
+sudo apt install openjdk-11-jdk -y
+
+# Download Tomcat 9
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
+
+# Extract Tomcat archive
+tar -xvzf apache-tomcat-9.0.80.tar.gz
+#changed directory
+cd ./apache-tomcat-9.0.80/bin
+#triggered script
+./shutdown.sh
+./startup.sh
+
+# Display instructions for accessing Tomcat
+echo "Tomcat 9 has been installed and started."
+echo "You can access it at http://localhost:8080/"
