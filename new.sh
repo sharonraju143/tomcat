3471255d0035eaf94252cf7a63836aa631086bda
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-08-31 14:18:42 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -6,6 +6,7 @@ sudo apt upgrade -y
 
 # Install Java 11
 sudo apt install openjdk-11-jdk -y
+# change ownership
 
 # Download Tomcat 9
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
@@ -14,6 +15,8 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.t
 tar -xvzf apache-tomcat-9.0.80.tar.gz
 #changed directory
 cd ./apache-tomcat-9.0.80/bin
+chmod +x ./apache-tomcat-9.0.80/bin/startup.sh 
+chmod +x ./apache-tomcat-9.0.80/bin/shutdown.sh
 #triggered script
 ./shutdown.sh
 ./startup.sh
