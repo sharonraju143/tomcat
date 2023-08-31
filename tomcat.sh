99c6b991108e4c931322cd5e9ec75e600130e318
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-08-31 11:49:59 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-# Update package list and install Java (if not already installed)
-sudo apt update
-sudo apt install default-jdk -y
+# Install necessary packages
+apt-get update
+apt-get install -y default-jdk
 
-# Create a Tomcat user
-sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+# Create a tomcat user and group
+groupadd tomcat
+useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 
-# Download Tomcat (adjust the version as needed)
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz -P /tmp
+# Download and extract Tomcat
+TOMCAT_VERSION="9.0.80"  # Update to the desired version
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz -P /tmp
+tar xf /tmp/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt/tomcat
 
-# Extract and move Tomcat files
-sudo tar xf /tmp/apache-tomcat-9.0.80.tar.gz -C /opt/tomcat
-sudo ln -s /opt/tomcat/apache-tomcat-9.0.80 /opt/tomcat/latest
+# Set proper ownership and permissions
+chown -R tomcat:tomcat /opt/tomcat/apache-tomcat-${TOMCAT_VERSION}
+chmod +x /opt/tomcat/apache-tomcat-${TOMCAT_VERSION}/bin/*.sh
 
-# Set ownership and permissions
-sudo chown -R tomcat:/opt/tomcat/latest
-sudo chmod +x /opt/tomcat/latest/bin/*.sh
-sudo chmod +x /opt/tomcat/latest/bin/startup.sh
-
-
-# Create a systemd service file for Tomcat
-echo "[Unit]
+# Create a systemd service file
+cat << EOF > /etc/systemd/system/tomcat.service
+[Unit]
 Description=Apache Tomcat Web Application Container
 After=network.target
 
 [Service]
 Type=forking
-
 Environment=JAVA_HOME=/usr/lib/jvm/default-java
-Environment=CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat/latest
-Environment=CATALINA_BASE=/opt/tomcat/latest
+Environment=CATALINA_PID=/opt/tomcat/apache-tomcat-${TOMCAT_VERSION}/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat/apache-tomcat-${TOMCAT_VERSION}
+Environment=CATALINA_BASE=/opt/tomcat/apache-tomcat-${TOMCAT_VERSION}
 Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-
-ExecStart=/opt/tomcat/latest/bin/startup.sh
-ExecStop=/opt/tomcat/latest/bin/shutdown.sh
-
+ExecStart=/opt/tomcat/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
+ExecStop=/opt/tomcat/apache-tomcat-${TOMCAT_VERSION}/bin/shutdown.sh
 User=tomcat
 Group=tomcat
 UMask=0007
@@ -44,9 +39,10 @@ RestartSec=10
 Restart=always
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/tomcat.service
+WantedBy=multi-user.target
+EOF
 
-# Reload systemd and start Tomcat service
-sudo systemctl daemon-reload
-sudo systemctl start tomcat
-sudo systemctl enable tomcat
+# Reload systemd and start Tomcat
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
