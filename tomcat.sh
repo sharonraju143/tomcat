c71b70eb8b976fa7576cd96c6a3042368a4da80e
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-08-31 10:30:26 +0530

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Update package list and install Java (if not already installed)
+sudo apt update
+sudo apt install default-jdk -y
+
+# Create a Tomcat user
+sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+
+# Download Tomcat (adjust the version as needed)
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.54/bin/apache-tomcat-9.0.54.tar.gz -P /tmp
+
+# Extract and move Tomcat files
+sudo tar xf /tmp/apache-tomcat-9.0.54.tar.gz -C /opt/tomcat
+sudo ln -s /opt/tomcat/apache-tomcat-9.0.54 /opt/tomcat/latest
+
+# Set ownership and permissions
+sudo chown -R tomcat: /opt/tomcat/latest
+sudo chmod +x /opt/tomcat/latest/bin/*.sh
+
+# Create a systemd service file for Tomcat
+echo "[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/lib/jvm/default-java
+Environment=CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat/latest
+Environment=CATALINA_BASE=/opt/tomcat/latest
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+
+ExecStart=/opt/tomcat/latest/bin/startup.sh
+ExecStop=/opt/tomcat/latest/bin/shutdown.sh
+
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/tomcat.service
+
+# Reload systemd and start Tomcat service
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl enable tomcat convert this to inline remote exec
